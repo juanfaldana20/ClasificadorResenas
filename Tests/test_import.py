c78812dfa0c302f8943b34f3509c22ba0b5dcae0
Juan Fernando Aldana <juanfaldana2000@gmail.com>
2025-09-17 20:50:41 -0500

    contrato gRPC v1 para predicción y batch

---
+++ Tests/test_import.py
@@ -0,0 +1,9 @@
+import sys
+
+# Agregamos la carpeta ml al path para que Python encuentre los módulos
+sys.path.append("ml")
+
+import sentiment_pb2
+import sentiment_pb2_grpc
+
+print("✅ Stubs importados correctamente")
